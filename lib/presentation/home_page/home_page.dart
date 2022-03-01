83036bf242f4715564cb4a806f331893eded80da
Soham Sawant <geekysoham@gmail.com>
2022-03-01 11:31:43 +0530

    created main header

--- lib/presentation/home_page/home_page.dart
+++ lib/presentation/home_page/home_page.dart
@@ -5,6 +5,30 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 16.0),
+            Text(
+              "March 9, 2020",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 32.0,
+              ),
+            ),
+            Text(
+              "5 incomplete ,5 completed",
+              style: TextStyle(
+                fontSize: 14.0,
+              ),
+            ),
+            SizedBox(height: 16.0),
+          ],
+        ),
+      ),
+    );
   }
 }
