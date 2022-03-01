24b4acd45d31b6de34dddef4e4ceed02fde564d3
Soham Sawant <geekysoham@gmail.com>
2022-03-01 19:55:01 +0530

    created full home page layout

--- lib/presentation/home_page/home_page.dart
+++ lib/presentation/home_page/home_page.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late ScrollController _incompleteController;
+  late ScrollController _completedController;
+
+  @override
+  void initState() {
+    super.initState();
+    _incompleteController = ScrollController();
+    _completedController = ScrollController();
+  }
+
+  @override
+  void dispose() {
+    _incompleteController.dispose();
+    _completedController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +63,30 @@ class HomePage extends StatelessWidget {
             SizedBox(height: size.height * 0.01992),
             Container(
               height: size.height * 0.359,
-              color: Colors.black,
+              child: ListView.builder(
+                controller: _incompleteController,
+                itemCount: 15,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding:
+                        EdgeInsets.fromLTRB(0, 0, 0, size.height * 0.01992),
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: size.width * 0.064,
+                          height: size.height * 0.0298,
+                          child: Checkbox(
+                            value: false,
+                            onChanged: (value) {},
+                          ),
+                        ),
+                        SizedBox(width: size.width * 0.0246),
+                        Text("Print parking Passes"),
+                      ],
+                    ),
+                  );
+                },
+              ),
             ),
             SizedBox(height: size.height * 0.0398),
             Text(
@@ -54,9 +99,45 @@ class HomePage extends StatelessWidget {
             SizedBox(height: size.height * 0.01992),
             Container(
               height: size.height * 0.229,
-              color: Colors.black,
+              child: ListView.builder(
+                controller: _completedController,
+                itemCount: 15,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding:
+                        EdgeInsets.fromLTRB(0, 0, 0, size.height * 0.01992),
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: size.width * 0.064,
+                          height: size.height * 0.0298,
+                          child: Checkbox(
+                            value: false,
+                            onChanged: (value) {},
+                          ),
+                        ),
+                        SizedBox(width: size.width * 0.0246),
+                        const Text("Print parking Passes"),
+                      ],
+                    ),
+                  );
+                },
+              ),
             )
           ],
+        ),
+      ),
+      floatingActionButton: SizedBox(
+        height: size.height * 0.0697,
+        width: size.width * 0.1493,
+        child: FloatingActionButton(
+          backgroundColor: const Color(0xff515CC6),
+          child: Icon(
+            Icons.add,
+            size: size.width * 0.0533,
+            color: Colors.white,
+          ),
+          onPressed: () {},
         ),
       ),
     );
